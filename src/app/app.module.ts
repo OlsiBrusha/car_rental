84f0fd74d3017e575e65701831225cefd667de9f
Olsi27 <120908636+Olsi27@users.noreply.github.com>
2023-06-10 10:54:36 +0200

    Merge branch 'login/register' of https://github.com/OlsiBrusha/car_rental into feature/homepage
    # Conflicts:
    #	src/app/app.component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContentComponent } from './content/content.component';
+import { BookingComponent } from './booking/booking.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-   
+   ContentComponent,
+   BookingComponent,
+   HomeComponent
     
   ],
   imports: [
